e321bba850a6df8d65a7e82de7b80129fb8a646f
CodingWithDeep18 <deep.shah@bacancy.com>
2023-06-23 17:10:28 +0530

    First React Application with useState

---
+++ first_react_app/src/Demo.jsx
@@ -0,0 +1,9 @@
+
+function Demo() {
+    return (
+      <>
+       <h1>Hey All, This is my first react application where you can increment and decrement the value by 1</h1>
+      </>
+    )
+  }
+export default Demo
