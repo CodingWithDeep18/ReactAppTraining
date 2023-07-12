ebafc977be983450875bbfed0e3b42210d49f771
CodingWithDeep18 <deep.shah@bacancy.com>
2023-07-12 13:47:14 +0530

    fetch-api-data using axios in react

--- fetch-api-data/src/UserList.jsx
+++ fetch-api-data/src/UserList.jsx
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function UserList() {
   const [users, setUsers] = useState([]);
   const [editingIndex, setEditingIndex] = useState(-1);
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    avatar: ''
+  });
 
   useEffect(() => {
-    fetch('https://reqres.in/api/users')
-      .then(response => response.json())
-      .then(data => setUsers(data.data));
+    axios.get('https://reqres.in/api/users')
+      .then(response => {
+        setUsers(response.data.data);
+      })
+      .catch(error => {
+        console.log('Fetch error:', error);
+        // Handle error or display an error message
+      });
   }, []);
 
   const handleEdit = (index) => {
@@ -16,41 +29,30 @@ function UserList() {
 
   const handleUpdate = (index) => {
     const editedUser = users[index];
-
-    fetch(`https://reqres.in/api/users/${editedUser.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(editedUser),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Update response:', data);
-        // Handle success or display a message
+    axios.put(`https://reqres.in/api/users/${editedUser.id}`, editedUser)
+      .then(response => {
+        console.log('Update response:', response.data);
         setEditingIndex(-1);
+      })
+      .catch(error => {
+        console.log('Update error:', error);
+        // Handle error or display an error message
       });
   };
 
   const handleDelete = (userId) => {
-    fetch(`https://reqres.in/api/users/${userId}`, {
-      method: 'DELETE',
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Delete response:', data);
-        // Handle success or display a message
+    axios.delete(`https://reqres.in/api/users/${userId}`)
+      .then(response => {
+        console.log('Delete response:', response.data);
         setUsers(prevUsers => prevUsers.filter(user => user.id !== userId));
-        setEditingIndex(-1); // Reset the editing index
+        setEditingIndex(-1);
       })
       .catch(error => {
         console.log('Delete error:', error);
         // Handle error or display an error message
       });
   };
-  
-  
-  
+
   const handleInputChange = (event, index) => {
     const { name, value } = event.target;
     setUsers(prevUsers => {
@@ -63,9 +65,76 @@ function UserList() {
     });
   };
 
+  const handleCreate = () => {
+    const newUser = {
+      ...formData,
+      id: users.length + 1 // Generate a temporary unique ID
+    };
+
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(response => {
+        console.log('Create response:', response.data);
+        setCreatingUser(false);
+        setFormData({
+          first_name: '',
+          last_name: '',
+          email: '',
+          avatar: ''
+        });
+        setUsers(prevUsers => [...prevUsers, newUser]);
+      })
+      .catch(error => {
+        console.log('Create error:', error);
+        // Handle error or display an error message
+      });
+  };
+
+  const renderCreateForm = () => {
+    if (creatingUser) {
+      return (
+        <div>
+          <h3>Create User</h3>
+          <input
+            type="text"
+            name="first_name"
+            value={formData.first_name}
+            onChange={(event) => setFormData({ ...formData, first_name: event.target.value })}
+            placeholder="First Name"
+          />
+          <input
+            type="text"
+            name="last_name"
+            value={formData.last_name}
+            onChange={(event) => setFormData({ ...formData, last_name: event.target.value })}
+            placeholder="Last Name"
+          />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={(event) => setFormData({ ...formData, email: event.target.value })}
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            name="avatar"
+            value={formData.avatar}
+            onChange={(event) => setFormData({ ...formData, avatar: event.target.value })}
+            placeholder="Avatar"
+          />
+          <button onClick={handleCreate}>Submit</button>
+          <button onClick={() => setCreatingUser(false)}>Cancel</button>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div>
       <h2>User List</h2>
+      <button onClick={() => setCreatingUser(true)}>Create User</button>
+      {renderCreateForm()}
       <table>
         <thead>
           <tr>
